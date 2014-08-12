25d5ae9239a76b18d5a050302bf94a8ba938cacc
andreykadatsky <andrey.kadatsky@mobindusry.net>
2014-08-12 10:04:45 +0300

    add fragments

---
+++ src/com/akadatsky/musiccharts/DenmarkFragment.java
@@ -0,0 +1,16 @@
+package com.akadatsky.musiccharts;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DenmarkFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.denmark_fragment, container, false);
+    }
+
+}
--- src/com/akadatsky/musiccharts/MainActivity.java
+++ src/com/akadatsky/musiccharts/MainActivity.java
@@ -1,15 +1,59 @@
 package com.akadatsky.musiccharts;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        ActionBar actionbar = getActionBar();
+        actionbar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        ActionBar.Tab worldTab = actionbar.newTab().setText(getString(R.string.world));
+        ActionBar.Tab ukraineTab = actionbar.newTab().setText(getString(R.string.ukraine));
+        ActionBar.Tab denmarkTab = actionbar.newTab().setText(getString(R.string.denmark));
+
+        Fragment worldFragment = new WorldFragment();
+        Fragment ukraineFragment = new UkraineFragment();
+        Fragment denmarkFragment = new DenmarkFragment();
+
+        worldTab.setTabListener(new MyTabsListener(worldFragment));
+        ukraineTab.setTabListener(new MyTabsListener(ukraineFragment));
+        denmarkTab.setTabListener(new MyTabsListener(denmarkFragment));
+
+        actionbar.addTab(worldTab);
+        actionbar.addTab(ukraineTab);
+        actionbar.addTab(denmarkTab);
     }
+
+    private class MyTabsListener implements ActionBar.TabListener {
+
+        public Fragment fragment;
+
+        public MyTabsListener(Fragment fragment) {
+            this.fragment = fragment;
+        }
+
+        @Override
+        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+            ft.replace(R.id.fragment_container, fragment);
+        }
+
+        @Override
+        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+            ft.remove(fragment);
+        }
+
+        @Override
+        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+        }
+    }
+
 }
